9383cc4b935b7b5fc1f13f472e9cc51f8afa30b8
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-06 13:45:12 +0700

    add code js

---
+++ React Native/Code/Day 2/js_intro.js
@@ -0,0 +1,74 @@
+///// Hello World
+var greeting = "Hello World";
+console.log(greeting);
+
+///// Data Type
+// {Number, String, Boolean}
+var name = "Arief Rachman Hakim";
+var number = 22;
+var isMale = true;
+
+console.log(name);
+console.log(number);
+console.log(isMale);
+
+//Hati-hati undefined
+var things;
+console.log(things);
+
+///// Operator
+///Aritmatika
+// (+) tambah
+// (-) kurang
+// (*) kali
+// (/) bagi
+// (%) modulus
+
+///Assingment
+var angka = 19;
+
+///Perbandingan
+//Equal (==)
+var number = 120;
+console.log(number == 100);
+
+//Not Equal ( != )
+var hobby = "Ngoding";
+console.log(hobby != "main game");
+console.log(hobby == "main game");
+
+// Strict Equal ( === )
+var number = 21;
+console.log(number == "21");
+console.log(number === "21");
+console.log(number === 21);
+
+// Strict Not Equal ( !==)
+var number = 22;
+console.log(number != "22");
+console.log(number !== "22");
+console.log(number !== 22);
+
+// Kurang dari & Lebih dari
+var number = 25;
+console.log(number < 20);
+console.log(number > 25);
+console.log(number <= 20);
+console.log(number >= 25);
+
+// Kondisional
+// AND ( && )
+var number = 29;
+console.log(number < 30 && number > 20);
+console.log(number < 35 && number >= 30);
+console.log(number >= 31 && number < 40);
+console.log(number > 29 && number < 30 && number > 28);
+console.log(number > 10 && number < 30 && number == 29);
+
+// OR ( || )
+var number = 17;
+console.log();
+console.log(number >= 17 || number < 20);
+console.log(number > 15 || number < 16);
+console.log(number > 10 || number < 12 || number == 14);
+console.log(number < 17 || number == 12);
